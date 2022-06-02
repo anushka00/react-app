27068b1fce5cfa7fc4ea47c373e5483d632cad0e
anushka00 <anushkashrivastava22@gmail.com>
2022-06-03 01:29:01 +0530

    added

---
+++ demo-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div class="text-center">Hi</div>
+    <h1 className='text-center'>Welcome to the REACT APP </h1>
+    
+    </>
+  );
+}
+
+export default App;
